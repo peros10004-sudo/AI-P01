f663950c16136810456d4fd7aa0ec88a92d1f3d7
peros10004-sudo <peros10004@gmail.com>
2025-11-03 13:03:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
-st.text_input('이름을 입력하세요:')
+name=st.text_input('이름을 입력하세요:')
 if st.button('인사말생성'):
-  st.write(name+_'님! 안녕하세요')
+  st.write(name+'님! 안녕하세요')
 
