2a779a903ca2166a92125342a9dee13b3b05417f
peros10004-sudo <peros10004@gmail.com>
2025-11-03 13:14:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
 name=st.text_input('이름을 입력하세요:')
+st.selectbox('좋아하는 음식을 선택해주세요:[김치찌개',된장찌개']
 if st.button('인사말생성'):
-  st.write(name+'님! 안녕하세요')
-  st.balloons()
+  st.info(name+'님! 안녕하세요')
+  st.warning('반가워요')
+ st.balloons()
+  
 
